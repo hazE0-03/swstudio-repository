52c7e9aa1c2550802bddba8760fe90210c6f5191
haze <hiromasa.hasegawa0228@gmail.com>
2024-03-20 00:26:22 +0900

    フォルダ構成及び、main.pyを変更しました。

--- myapp/main.py
+++ myapp/main.py
@@ -1,9 +1,12 @@
-from modules import app
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
-
+app = FastAPI()
 templates = Jinja2Templates(directory="templates")
+
 @app.get("/", response_class=HTMLResponse)
-async def index():
-    return templates.TemplateResponse("index.html")
+async def read_root(request: Request):
+    context = {"request": request}
+    return templates.TemplateResponse("index.html", context)
