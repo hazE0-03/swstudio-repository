98b1fd91d2b13b9ce3768a24b74080b086ab28d1
haze <hiromasa.hasegawa0228@gmail.com>
2024-03-22 19:06:38 +0900

    データベースを連携させ、新規作成機能、編集機能、削除機能を実装しました。

--- myapp/db.py
+++ myapp/db.py
@@ -1,11 +1,11 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, inspect
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, DateTime
 from datetime import datetime
 import pytz
 
-engine = create_engine("sqlite3:///database.db", echo=True)
+engine = create_engine("sqlite:///database.db", echo=True)
 db_session = scoped_session(
     sessionmaker(
         autocommit=False,
@@ -13,45 +13,47 @@ db_session = scoped_session(
         bind=engine
     )
 )
-
 Base = declarative_base()
 Base.query = db_session.query_property()
 Base.metadata.create_all(bind=engine)
+
 class Character(Base):
     __tablename__ = "character_class"
     id = Column(Integer, primary_key=True)
     name = Column(String(50))
     impurity = Column(Integer)
-    race = Column(String(15))
-    subrace = Column(String(15))
-    birth = Column(String(10))
+    # race = Column(String(15))
+    # subrace = Column(String(15))
+    # birth = Column(String(10))
     dexterity = Column(Integer)
     agility = Column(Integer)
     strength = Column(Integer)
     vitality = Column(Integer)
     intelligence = Column(Integer)
     spirit = Column(Integer)
-    fighter = Column(Integer)
-    grappler = Column(Integer)
-    fencer = Column(Integer)
-    shooter = Column(Integer)
-    battledancer = Column(Integer)
-    sorcerer = Column(Integer)
-    conjurer = Column(Integer)
-    priest = Column(Integer)
-    fairytamer = Column(Integer)
-    magitech = Column(Integer)
-    demonruler = Column(Integer)
-    druid = Column(Integer)
-    scout = Column(Integer)
-    ranger = Column(Integer)
-    enhancer = Column(Integer)
-    bird = Column(Integer)
-    rider = Column(Integer)
-    alchemist = Column(Integer)
-    warleader = Column(Integer)
-    geomancer = Column(Integer)
+    # fighter = Column(Integer)
+    # grappler = Column(Integer)
+    # fencer = Column(Integer)
+    # shooter = Column(Integer)
+    # battledancer = Column(Integer)
+    # sorcerer = Column(Integer)
+    # conjurer = Column(Integer)
+    # priest = Column(Integer)
+    # fairytamer = Column(Integer)
+    # magitech = Column(Integer)
+    # demonruler = Column(Integer)
+    # druid = Column(Integer)
+    # scout = Column(Integer)
+    # ranger = Column(Integer)
+    # enhancer = Column(Integer)
+    # bird = Column(Integer)
+    # rider = Column(Integer)
+    # alchemist = Column(Integer)
+    # warleader = Column(Integer)
+    # geomancer = Column(Integer)
     memo = Column(String)
     user_name = Column(String(50))
-    update_time = Column(DateTime, default=datetime.now(pytz.timezone('Asia/Tokyo')), onupdate=datetime.now(pytz.timezone('Asia/Tokyo')), nullable=False)
+    update_time = Column(DateTime)
 
+if not inspect(engine).has_table("character_class"):
+    Base.metadata.create_all(bind=engine, tables=[Character.__table__])
--- myapp/main.py
+++ myapp/main.py
@@ -1,22 +1,19 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from sqlalchemy.orm import Session
 from random import randint
+from db import db_session, Character
+from datetime import datetime
+import pytz
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
-    characters = [
-        {"image_url": "画像1-png",
-         "name": "トルト・リスベール",
-         "update_time": "2024-3-18"},
-        {"image_url": "画像2-png",
-         "name": "レブン・マクマエリ",
-         "update_time": "2024-3-19"},
-    ]
+    characters = db_session.query(Character).all()
     return templates.TemplateResponse(
         "index.html",
         {"request": request, "characters": characters}
@@ -24,17 +21,63 @@ async def index(request: Request):
 
 @app.get("/create")
 async def create(request: Request):
-    # race_features = {"人間":"[剣の加護／運命変転]",
-    # "エルフ": "[暗視][剣の加護／優しき水]",
-    # "ドワーフ":"[暗視][剣の加護／炎身]"},
     return templates.TemplateResponse(
         "create.html",
         {"request": request}
     )
 
-# @app.post("/create")
-# async def create(request: Request):
-#     return templates.TemplateResponse(
-#         "create.html",
-#         {"request": request}
-#     )
+@app.post("/create")
+async def create(request: Request):
+    form = await request.form()
+    current_time = datetime.now(pytz.timezone('Asia/Tokyo'))
+    update_time = current_time
+    name = form["name"]
+    impurity = form["impurity"]
+    dexterity = form["dexterity"]
+    agility = form["agility"]
+    strength = form["strength"]
+    vitality = form["vitality"]
+    intelligence = form["intelligence"]
+    spirit = form["spirit"]
+    memo = form["memo"]
+
+
+    character = Character(name=name, impurity=impurity, dexterity=dexterity, agility=agility, strength=strength, vitality=vitality, intelligence=intelligence, spirit=spirit, memo=memo, update_time=update_time)
+
+    db_session.add(character)
+    db_session.commit()
+    return RedirectResponse("/", status_code=303)
+
+@app.get("/{id:int}/update")
+async def update(request: Request, id: int):
+    character = Character.query.get(id)
+    return templates.TemplateResponse(
+        "update.html",
+        {"request": request, "character": character}
+    )
+
+@app.post("/{id:int}/update")
+async def update(request: Request, id: int):
+    character = Character.query.get(id)
+    form = await request.form()
+    current_time = datetime.now(pytz.timezone('Asia/Tokyo'))
+    character.update_time = current_time
+    character.name = form["name"]
+    character.impurity = form["impurity"]
+    character.dexterity = form["dexterity"]
+    character.agility = form["agility"]
+    character.strength = form["strength"]
+    character.vitality = form["vitality"]
+    character.intelligence = form["intelligence"]
+    character.spirit = form["spirit"]
+    character.memo = form["memo"]
+
+    db_session.commit()
+    return RedirectResponse("/", status_code=303)
+
+@app.get("/{id:int}/delete")
+async def delete(request: Request, id: int):
+    character = Character.query.get(id)
+    db_session.delete(character)
+    db_session.commit()
+    return RedirectResponse("/", status_code=303)
